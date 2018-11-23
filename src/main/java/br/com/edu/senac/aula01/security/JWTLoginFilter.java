2e3e0ef6f9624eff566ccdef9ac06d1fdb22a646
Fabiano Oss <fabiano.oss@gmail.com>
2018-11-23 21:51:16 -0200

    Segurança

---
+++ src/main/java/br/com/edu/senac/aula01/security/JWTLoginFilter.java
@@ -0,0 +1,52 @@
+package br.com.edu.senac.aula01.security;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import javax.security.sasl.AuthenticationException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JWTLoginFilter extends AbstractAuthenticationProcessingFilter {
+
+	protected JWTLoginFilter(String url, AuthenticationManager authManager) {
+		super(new AntPathRequestMatcher(url));
+		setAuthenticationManager(authManager);
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException, IOException, ServletException {
+		
+		Usuario credentials = new ObjectMapper()
+				.readValue(request.getInputStream(), Usuario.class);
+		
+		return getAuthenticationManager().authenticate(
+				new UsernamePasswordAuthenticationToken(
+						credentials.getNome(), 
+						credentials.getSenha(), 
+						Collections.emptyList()
+						)
+				);
+	}
+	
+	@Override
+	protected void successfulAuthentication(
+			HttpServletRequest request, 
+			HttpServletResponse response,
+			FilterChain filterChain,
+			Authentication auth) throws IOException, ServletException {
+		
+		TokenAuthenticationService.addAuthentication(response, auth.getName());
+	}
+}
